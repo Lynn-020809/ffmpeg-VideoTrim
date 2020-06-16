2365ce4d6afd8cd4d86cbede13870fca00b83d30
Lynn-020809 <61681936+Lynn-020809@users.noreply.github.com>
2020-06-17 02:15:45 +0800

    Update TrimInfo.py

--- TrimInfo.py
+++ TrimInfo.py
@@ -6,8 +6,8 @@ TrimInfo = namedtuple('TrimStruct','infile initial_time terminal_time outfile')
 
 def triminfo(path,video_time):
     list_i=[]
-    TrimInfo.infile = path.strip()
-    time_name = re.findall("BV(.*?).mp4",TrimInfo.infile)
+    infile = path.strip()
+    time_name = re.findall("BV(.*?).mp4",infile)
     pattern_i = re.compile(time_name[0]+':i:"(.*?)"')
     pattern_t = re.compile(time_name[0]+':t:"(.*?)"')
     for line in video_time:
@@ -15,9 +15,9 @@ def triminfo(path,video_time):
             list_i = re.findall(pattern_i,line)
         if(re.findall(pattern_t,line)!=[]):
             list_t = re.findall(pattern_t,line)
-    TrimInfo.initial_time = list_i[0].split(',')
-    TrimInfo.terminal_time = list_t[0].split(',')
-    length = len(TrimInfo.initial_time)
+    initial_time = list_i[0].split(',')
+    terminal_time = list_t[0].split(',')
+    length = len(initial_time)
     for i in range(length):
-        TrimInfo.outfile = str(i)+ path
-    return TrimInfo(TrimInfo.infile, TrimInfo.initial_time, TrimInfo.terminal_time, TrimInfo.outfile)
+        outfile = str(i)+ path
+    return TrimInfo(infile, initial_time, terminal_time, outfile)
